e134a72b6d1f30fb7063b7fd7da190fa23c5482e
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-10 19:06:39 +0545

    first commit

---
+++ frontend/src/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../component/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-center mt-10 text-3xl font-bold">Gamvir Samudra Setu Secondary School</h1>
+        <h1 className=" text-center mt-10 text-red-500">This website is under devlopment.</h1>
+        <h2 className="text-center ">For more information please vist us at school or contact us through email: gamvir09@gmail.com</h2>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
