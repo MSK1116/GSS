2a6ea14461a015c83e82a4f73b914ee0144c80f5
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-11 12:50:46 +0545

    push

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
                 <span className="badge badge-sm indicator-item">8</span>
               </div>
             </div>
-            <div tabIndex={0} className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+            {/* <div tabIndex={0} className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
               <div className="card-body">
                 <span className="text-lg font-bold">8 Items</span>
                 <span className="text-info">Subtotal: $999</span>
@@ -57,7 +57,7 @@ const Navbar = () => {
                   <button className="btn btn-primary btn-block">View cart</button>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
           <button className="btn hidden md:block ">Contact Us</button>
         </div>
