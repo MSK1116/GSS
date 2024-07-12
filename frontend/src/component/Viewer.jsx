04e741bde96f1a5ee595901b42a062bfc000c81a
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-12 21:07:44 +0545

    push

--- frontend/src/component/Viewer.jsx
+++ frontend/src/component/Viewer.jsx
@@ -6,8 +6,8 @@ import { Carousel } from "react-responsive-carousel";
 const Viewer = () => {
   return (
     <>
-      <div className="absolute md:-top-36 -top-28 -left-28 overflow-hidden  z-0   ">
-        <svg className="md:w-96 w-80 z-0 h-full object-fill fill-yellow-400 " viewBox="0 0 500 670" xmlns="http://www.w3.org/2000/svg">
+      <div className="absolute md:-top-36 -top-28 md:-left-28 -left-24 overflow-hidden  z-0   ">
+        <svg className="md:w-96 w-80 z-0 h-full object-fill opacity-70 md:opacity-85 fill-yellow-400 " viewBox="0 0 500 670" xmlns="http://www.w3.org/2000/svg">
           <ellipse
             style={{
               strokeWidth: 0,
@@ -19,7 +19,7 @@ const Viewer = () => {
           />
         </svg>
       </div>
-      <span className="md:text-5xl text-xs flex flex-col absolute top-20 left-1 z-10  text-center text-white">
+      <span className="md:text-5xl text-xs flex flex-col absolute md:top-20  top-24 left-1 z-10  text-center text-white">
         71
         <span className="md:text-sm text-xs text-white left-6">years of excellence!</span>
       </span>
