31c48450c7c48bc66de4452b8ae80718b877d8e7
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-14 17:52:39 +0545

    xsx

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,28 +1,59 @@
 import React from "react";
+import { FaMailBulk, FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
     <>
-      <footer className="max-w-screen-2xl container mx-auto md:px-20px  footer  footer-center bg-yellow-300 text-white  p-10">
-        <aside>
-          <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="inline-block fill-current">
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p className="font-bold  text-3xl">
-            GSS Sec. School
-            <br />
-            <span className=" text-sm">"Imadol, Lalitpur, Nepal"</span>
-          </p>
-          <div>
-            <h1 className="text-center "> Thanks for finding us..! we will update this portal in future.</h1>
-            <h3 className="text-center">
-              For technical update contact GSS alumni:{" "}
-              <a className=" underline" href="https://manishmahato.info.np">
-                contact@msmahato.com.np
-              </a>
-            </h3>
+      <footer className="max-w-screen-2xl p-8 container mx-auto md:px-20px  footer  footer-center bg-yellow-300 text-white  ">
+        <div className="flex flex-row w-full justify-between">
+          <div className=" text-left  overflow-hidden">
+            <img
+              className=" object-cover mb-4 rounded-full h-36 w-36 hover:scale-105 transition-all duration-700"
+              src="https://images.squarespace-cdn.com/content/v1/5e0849d5b75e913537ba6e4b/1580072657793-FQHY1078YO7V1B6O3A5C/Target%2BLogo%2B_%2BPictorial%2BLogo%2BExample%2B_%2BMill%2BCreek%2BCreative.png"
+              alt="photo"></img>
+            <h1 className=" font-extrabold">GSS Sec. School</h1>
+            <h1>Mahalaxmi Nagarpalika-2, Imadol, Lalitpur, Nepal</h1>
           </div>
-        </aside>
+          <div className="flex flex-col  text-left">
+            <h1 className=" text-blue-600  text-[18px] ">Quick Link</h1>
+            <a>Contact us</a>
+            <a>Result</a>
+            <a>Apply</a>
+          </div>
+          <div className="flex flex-col  text-left">
+            <h1 className=" text-blue-600 mb-4 text-[18px] ">Quick contact</h1>
+
+            <span className="flex flex-row items-center ">
+              <FaPhoneAlt className="w-6 h-6" /> +01 5201303
+            </span>
+            <span className="flex flex-row items-center ">
+              <FaPhoneAlt className="w-6 h-6" /> +01 5204323
+            </span>
+
+            <span className="flex flex-row items-center ">
+              <MdEmail className="w-6 h-6" /> gamvir09@gmail.com
+            </span>
+            <span className="flex flex-row items-center ">
+              <MdEmail className="w-6 h-6" /> gamvirengdep073@gmail.com
+            </span>
+          </div>
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.5071563018428!2d85.34102777468506!3d27.670715976203564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19e55e6303d1%3A0x307d48bbab7bf1bc!2sGamvir%20Samudra%20Setu%20H.%20S.%20School!5e0!3m2!1sen!2snp!4v1720957027709!5m2!1sen!2snp"
+              width="200"
+              height="200"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+        <h1 className=" flex flex-row">
+          Crafted by GSS alumni:
+          <a target="blank" href="https://www.manishmahato.info.np/">
+            Manish Singh Mahato
+          </a>
+        </h1>
       </footer>
     </>
   );
