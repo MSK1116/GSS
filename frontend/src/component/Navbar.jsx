b96a9417bdf8f1a9bb3e6968d50521358757ca34
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-14 21:33:43 +0545

    push

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -35,8 +35,14 @@ const Navbar = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm gap-2 dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             {navItems}
+            <Link to={"/apply"}>
+              <li>Apply</li>
+            </Link>
+            <Link to={"/contact"}>
+              <li>Contact</li>
+            </Link>
           </ul>
         </div>
         <div className="navbar-center hidden mr-5 lg:flex">
