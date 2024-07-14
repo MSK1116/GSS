e0788cbf38541ef39d34b8f5b4512e6dd129a376
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-14 09:33:39 +0545

    push

--- frontend/src/component/Program.jsx
+++ frontend/src/component/Program.jsx
@@ -1,45 +1,16 @@
 import React from "react";
+import ProgramList from "../assets/Program.json";
+import Programcard from "./Programcard";
 
 const Program = () => {
   return (
     <>
-      <section className="max-w-screen-2xl p-8 bg-gray-50 gap-4 ">
+      <section className="max-w-screen-2xl p-8 bg-gray-50  ">
         <h1 className=" text-3xl mb-4  font-bold text-red-500">Our Programs</h1>
-        <div className=" grid grid-cols-2 grid-rows-3">
-          <div>
-            <article className="flex bg-white transition hover:shadow-xl">
-              <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
-                <time datetime="2022-10-10" className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900">
-                  <span>2022</span>
-                  <span className="w-px flex-1 bg-gray-900/10"></span>
-                  <span>Oct 10</span>
-                </time>
-              </div>
-
-              <div className="hidden sm:block sm:basis-56">
-                <img alt="" src="https://images.unsplash.com/photo-1609557927087-f9cf8e88de18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" className="aspect-square h-full w-full object-cover" />
-              </div>
-
-              <div className="flex flex-1 flex-col justify-between">
-                <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-                  <a href="#">
-                    <h3 className="font-bold uppercase text-gray-900">Finding the right guitar for your style - 5 tips</h3>
-                  </a>
-
-                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta, voluptates neque
-                    explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?
-                  </p>
-                </div>
-
-                <div className="sm:flex sm:items-end sm:justify-end">
-                  <a href="#" className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400">
-                    Read Blog
-                  </a>
-                </div>
-              </div>
-            </article>
-          </div>
+        <div className=" grid grid-cols-2 grid-rows-3  space-x-4 gap-y-4">
+          {ProgramList.map((ProgramList) => (
+            <Programcard key={ProgramList.id} items={ProgramList} />
+          ))}
         </div>
       </section>
     </>
---
+++ frontend/src/component/Programcard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Programcard = ({ items }) => {
+  return (
+    <>
+      <div>
+        <article className="flex bg-white transition hover:shadow-xl">
+          <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
+            <time className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900">
+              <span>{items.info1}</span>
+              <span className="w-px flex-1 bg-gray-900/10"></span>
+              <span>{items.info2}</span>
+            </time>
+          </div>
+
+          <div className="hidden sm:block sm:basis-56">
+            <img alt="" src={items.thumb} className="aspect-square h-full w-full object-cover" />
+          </div>
+          <div className="flex flex-1 flex-col justify-between">
+            <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
+              <a href="#">
+                <h3 className="font-bold uppercase text-gray-900">{items.title}</h3>
+              </a>
+
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">{items.description}</p>
+            </div>
+
+            <div className="sm:flex sm:items-end sm:justify-end">
+              <a href={items.link} className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400">
+                Read More
+              </a>
+            </div>
+          </div>
+        </article>
+      </div>
+    </>
+  );
+};
+
+export default Programcard;
