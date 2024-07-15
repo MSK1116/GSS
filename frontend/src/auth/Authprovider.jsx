fd4d62d13d3e9e526bc7a8feaa0ff871d1bcadb0
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-15 17:26:40 +0545

    sa

--- frontend/src/auth/Authprovider.jsx
+++ frontend/src/auth/Authprovider.jsx
@@ -4,8 +4,11 @@ export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
   const envID = import.meta.env.VITE_ID;
+  const jsonObj = { envID };
+  const ID = JSON.stringify(jsonObj);
+
   const initailAuth = localStorage.getItem("ID");
-  const [authUser, setAuthUser] = useState(initailAuth == envID ? JSON.parse(initailAuth) : undefined);
+  const [authUser, setAuthUser] = useState(initailAuth == ID ? JSON.parse(initailAuth) : undefined);
   console.log(authUser);
   return <AuthContext.Provider value={[authUser, setAuthUser]}>{children}</AuthContext.Provider>;
 }
