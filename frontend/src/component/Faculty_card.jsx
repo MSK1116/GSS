c612bf29329c1a5eb18c08549003d0f01167ae6c
MANU0519 <anishkushwaga1116@gmail.com>
2024-08-12 14:28:56 +0545

    gf

--- frontend/src/component/Faculty_card.jsx
+++ frontend/src/component/Faculty_card.jsx
@@ -6,20 +6,23 @@ const Faculty_card = ({ items }) => {
   useEffect(() => {
     Aos.init();
     Aos.refresh();
-  });
+  }, []);
+
   return (
     <>
-      <div data-aos="zoom-out-up" data-aos-duration="600" className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center hover:shadow-lg">
-        <div data-aos="zoom-out" data-aos-duration="650" className="mb-8 overflow-hidden">
-          <img className="object-center object-cover rounded-full h-36 w-36 hover:scale-105 transition-all duration-700" src={items.photo} alt="photo"></img>
-        </div>
-        <div className="text-center">
-          <p data-aos="fade-up" className="text-xl text-gray-700 font-bold mb-2">
-            {items.title}
-          </p>
-          <p data-aos="fade-up" data-aos-delay="100" className="text-base text-gray-400 font-normal">
-            {items.post}
-          </p>
+      <div>
+        <div data-aos="zoom-out-up" data-aos-duration="600" className="w-full rounded-lg p-12 flex flex-col justify-center items-center hover:shadow-lg">
+          <div data-aos="zoom-out" data-aos-duration="650" className="mb-8 overflow-hidden">
+            <img className="object-center object-cover rounded-full h-36 w-36 " src={items.photo} alt="photo"></img>
+          </div>
+          <div className="text-center">
+            <p data-aos="fade-up" className="text-xl text-gray-700 font-bold mb-2">
+              {items.title}
+            </p>
+            <p data-aos="fade-up" data-aos-delay="100" className="text-base text-gray-400 font-normal">
+              {items.post}
+            </p>
+          </div>
         </div>
       </div>
     </>
--- frontend/src/faculty/Faculty2.jsx
+++ frontend/src/faculty/Faculty2.jsx
@@ -5,15 +5,15 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 const Faculty2 = () => {
+  const cardFac = List.filter((data) => data.slider != "true" && data.dept != "eng");
+  const cardFac1 = List.filter((data) => data.slider != "true" && data.dept == "eng");
   useEffect(() => {
     Aos.init();
     Aos.refresh();
-  });
-  const cardFac = List.filter((data) => data.slider != "true" && data.dept != "eng");
-  const cardFac1 = List.filter((data) => data.slider != "true" && data.dept == "eng");
+  }, []);
   return (
     <>
-      <section data-aos="zoom-in" className="max-w-screen-2xl bg-gray-50 mx-auto p-8 sm:px-6 lg:px-4 py-12 ">
+      <section className="max-w-screen-2xl   mx-auto p-8 sm:px-6 lg:px-4 py-12 ">
         <div className="text-center pb-12">
           <h2 data-aos="fade-down" data-aos-duration="600" className="text-base font-bold text-indigo-600">
             They are ready to give their time to you!
@@ -22,7 +22,7 @@ const Faculty2 = () => {
             Meet our teachers & staff
           </h1>
         </div>
-        <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5  gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-5  gap-6">
           {cardFac.map((cardFac) => (
             <Faculty_card key={cardFac.id} items={cardFac} />
           ))}
@@ -32,7 +32,7 @@ const Faculty2 = () => {
             Engineering Department
           </h1>
         </div>
-        <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5  gap-6">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5  gap-6">
           {cardFac1.map((cardFac1) => (
             <Faculty_card key={cardFac1.id} items={cardFac1} />
           ))}
